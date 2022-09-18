bf5f6b72d50b302674d36e1a9178ed88ebfbe046
Justin Tan <justintws10@gmail.com>
2022-09-18 16:57:12 +1000

    attempt on login and jwt

--- src/App.js
+++ src/App.js
@@ -1,25 +1,125 @@
 import logo from './logo.svg';
+import React from 'react'
 import './App.css';
+import axios from 'axios';
+import {Route, Link, HashRouter as Router} from 'react-router-dom'
+import Login from './components/Login';
+import MyProfile from './components/MyProfile';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const BASE_URL = 'http://localhost:3000'
+
+class App extends React.Component{
+
+  // App state
+  state ={
+    currentUser: undefined
+  }
+
+  // function to run on component mounting
+  componentDidMount(){
+
+    // this function will load once you load the website. We want to check if the user is logged in when we visit so we'll pass in the setCurrentUser function
+    this.setCurrentUser() 
+  }
+  
+  // function to set the state to the current logged in user
+  // This is the function to get the current user from your database if there is one.
+  // We declare that there is a token which holds a json web token 'jwt' from your local storage.
+  // We then do an axios request to the back end and ask it if we're logged in already. 
+  // We pass through this token as an authenticator header which let our server validate us.
+  // If our token is valid then we set the state to our current user. If not you'll see a warning in your console that you're unauthorized
+  setCurrentUser = () => {
+    let token = "Bearer " + localStorage.getItem("jwt");
+    axios.get(`${BASE_URL}/users/current`, {
+      headers: {
+        'Authorization': token
+      }
+    })
+    .then( res => {
+      this.setState({currentUser: res.data})
+    })
+    .catch(err => console.warn(err))
+  }
+
+  // function to log the user out
+  // Three things we need to do here.
+  // 1. Set our state of current user to undefined.
+  // 2. Remove the jwt token from our local storage
+  // 3. Set our axios default headers to undefined.
+
+  // This will completely reset our logged in state
+  handleLogout = () => {
+    this.setState({currentUser: undefined})
+    localStorage.removeItem("jwt");
+    axios.defaults.headers.common['Authorization'] = undefined;
+  }
+
+
+  // This is where all our HTML goes
+
+  // This is something like to notify whether a user is logged in or not (mostly at the home page), or a display text saying "Welcome, username"
+  render(){
+
+    return (
+      <Router>
+        <header>
+          <nav>
+            {/* Show one of two nav bars depending on if the user is logged in */}
+              {
+                this.state.currentUser !== undefined
+                ?
+                (
+                  <ul>
+                    <li>Welcome {this.state.currentUser.name} | </li>
+                    <li><Link to='/my_profile'>My Profile</Link></li>
+                    <li><Link onClick={this.handleLogout} to ='/'>Logout</Link></li>
+                  </ul>
+                )
+                :
+                (
+                  <ul>
+                    <li><Link to='/login'>Login</Link></li>
+                  </ul>
+                )
+              }
+
+          </nav>
+    
+        </header>
+
+        <Route exact path='/my_profile' component={MyProfile} />
+        <Route 
+          exact path='/login'
+          render={(props) => <Login setCurrentUser={this.setCurrentUser}{...props}/>} // Ask Luke & Kris what's this?
+        />
+
+      </Router>
+    )
+
+  }
+
 }
+
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
 
 export default App;
---
+++ src/components/MyProfile.jsx
@@ -0,0 +1,43 @@
+import React from 'react'
+import axios from 'axios'
+
+const BASE_URL = 'http://localhost:3000'
+
+// Component to show details about the user.
+
+class MyProfile extends React.Component {
+    state = {
+        currentUser: {
+            name: '',
+            email: ''
+        }
+    }
+
+    componentDidMount(){
+        let token = "Bearer " + localStorage.getItem("jwt")
+        axios.get(`${BASE_URL}/users/current`, {
+            headers: {
+                'Authorization': token
+            }
+        })
+        .then(res => {
+            this.setState({currentUser: res.data})
+        })
+        .catch(err => console.warn(err))
+    }
+
+    render() {
+
+        return (
+            <div>
+                <h1>Helloo {this.state.currentUser.name}</h1>
+                <h3>Your email is {this.state.currentUser.email}</h3>
+
+            </div>
+        )
+
+    }
+
+}
+
+export default MyProfile
