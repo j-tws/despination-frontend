040e9ff900686e936928811815c772071ab032a4
Justin Tan <justintws10@gmail.com>
2022-09-22 00:58:09 +1000

    tried bounding box

--- src/components/ReactMap.jsx
+++ src/components/ReactMap.jsx
@@ -27,6 +27,38 @@ function MyMarker( props ){
   )
 }
 
+// Return map bounds based on list of places
+const getMapBounds = (map, maps, places) => {
+  const bounds = new maps.LatLngBounds();
+
+  places.forEach((place) => {
+    bounds.extend(new maps.LatLng(
+      place.geometry.location.lat,
+      place.geometry.location.lng,
+    ));
+  });
+  return bounds;
+};
+
+// Re-center map when resizing the window
+const bindResizeListener = (map, maps, bounds) => {
+  maps.event.addDomListenerOnce(map, 'idle', () => {
+    maps.event.addDomListener(window, 'resize', () => {
+      map.fitBounds(bounds);
+    });
+  });
+};
+
+// Fit map to its bounds after the api is loaded
+const apiIsLoaded = (map, maps, places) => {
+  // Get bounds by our places
+  const bounds = getMapBounds(map, maps, places);
+  // Fit map to bounds
+  map.fitBounds(bounds);
+  // Bind the resize listener
+  bindResizeListener(map, maps, bounds);
+};
+
 class ReactMap extends React.Component {
 
   state = {
@@ -338,7 +370,7 @@ class ReactMap extends React.Component {
           }
         ]
       }
-    ]
+    ]    
 
     return (
       <div>
@@ -352,6 +384,8 @@ class ReactMap extends React.Component {
             defaultCenter={ {lat: 2, lng:28} }
             defaultZoom={ 1 }
             options={{styles: mapOptions}} // this for the customised google map
+            yesIWantToUseGoogleMapApiInternals
+            onGoogleApiLoaded={({ map, maps }) => apiIsLoaded(map, maps, this.state.destinations)}
           >
             {/* can wrap component around a div */}
             {/*
